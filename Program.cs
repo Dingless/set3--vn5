8aaf297a3176fb009b0bc74652878b95924189ea
Dingless <mohameddeg.abdulahi@elev.ga.ntig.se>
2024-02-05 13:39:58 +0100

    arean för en cirkel med hjälp av att ha radien som en parameter

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class program
+{
+    public static void Main(string[] args)
+    {
+        double radius = 1.5; // radien av cirkeln
+        double area = cirkelnsarea(radius); // använder metoden cirkelnsarea där radien är en parameter och resultatet har variabeln area
+        Console.WriteLine("Cirkelns area är" + area);    
+    }
+    public static double cirkelnsarea(double radius)  // metod som räknar ut cirkelns area med hjälp av radius
+    {
+        return Math.PI * radius * radius;  // ger tilbaks arean av cirkeln i konsolen
+    }
+
+
+}
